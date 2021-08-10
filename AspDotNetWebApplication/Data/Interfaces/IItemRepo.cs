4a25c6368415c29acd12bd222cb164a71ba343ff
Petronik <petronik@ukr.net>
2021-08-10 13:00:11 -0500

    Add project files.

---
+++ AspDotNetWebApplication/Data/Interfaces/IItemRepo.cs
@@ -0,0 +1,18 @@
+﻿using AspDotNetWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetWebApplication.Data.Interfaces
+{
+    interface IItemRepo
+    {
+        IEnumerable<Item> GetAllItems();
+        Item GetItemById(int id); //Read
+        void CreateItem(Item input); //Create
+        void UpdateItem(Item input); //Update
+        void DeleteItem(int id); //Delete
+        bool SaveChanges();
+    }
+}
---
+++ AspDotNetWebApplication/Data/MockRepo/MockItemRepo.cs
@@ -0,0 +1,51 @@
+﻿using AspDotNetWebApplication.Data.Interfaces;
+using AspDotNetWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetWebApplication.Data.MockRepo
+{
+    public class MockItemRepo : IItemRepo
+    {
+        private static List<Item> _items = new List<Item>
+        {
+            new Item{ Id = 101, Name = "Boiled Water", Price = 15.55},
+            new Item{ Id = 102, Name = "Fresh Stones", Price = 19.99},
+            new Item{ Id = 101, Name = "Boiled Water", Price = 2.22}
+        };
+        public void CreateItem(Item input)
+        {
+            _items.Add(input);
+        }
+
+        public void DeleteItem(int id)
+        {
+            var itemToDelete = _items.FirstOrDefault(i => i.Id == id);
+            if (itemToDelete != null)
+                _items.Remove(itemToDelete);
+        }
+
+        public IEnumerable<Item> GetAllItems()
+        {
+            return _items;
+        }
+
+        public Item GetItemById(int id)
+        {            
+            return _items.FirstOrDefault(i => i.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item input)
+        {
+            var itemToUpdate = _items.FirstOrDefault(i => i.Id == input.Id);
+
+        }
+    }
+}
