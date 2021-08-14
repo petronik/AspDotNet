b5af8b33fc24b57b9a715f02e9062bbd2989a661
Petronik <petronik@ukr.net>
2021-08-14 00:31:55 -0500

    Added Product Controler

---
+++ AspDotNetWebApplication/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using AspDotNetWebApplication.Data.Interfaces;
+using AspDotNetWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetWebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepo _productRepo;
+        private readonly IVendorRepo _vendorRepo;
+
+        public ProductController(IProductRepo prodRepo, IVendorRepo vendorRepo)
+        {
+            _productRepo = prodRepo;
+            _vendorRepo = vendorRepo;
+        }
+        public IActionResult Index()
+        {
+            var vendors = _vendorRepo.GetAllVendors();
+            var list = _productRepo.GetAllProducts()
+                .Select(p =>
+                {
+                    p.Vendor = vendors
+                                    .Where(v => v.V_code == p.V_code)
+                                    .FirstOrDefault() ?? new Vendor { 
+                                        V_name = "n/a"
+                                    };
+                    return p;
+                })
+                .ToList();
+            return View(list);
+        }
+    }
+}
